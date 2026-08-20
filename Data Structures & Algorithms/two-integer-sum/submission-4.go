3b46f42a143d45ab744886f29a83bb9a00312e0e
добрый кот <116643891+Damirka228@users.noreply.github.com>
2026-08-20 19:26:46 +0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.go
@@ -0,0 +1,13 @@
+func twoSum(nums []int, target int) []int {
+    response := []int{}
+	for i := 0; i < len(nums); i++ {
+		for j := 0; j < len(nums); j ++{
+			if nums[i] + nums[j] == target && i != j{
+				response = append(response, i, j)
+			}
+		}
+	}
+	return response[:2]
+}
+
+
